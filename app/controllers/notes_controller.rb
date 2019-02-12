a0a82f40b1f9792d8c166ccc968823c01b49a1a4
Brendon Stanton <brendonstanton@icloud.com>
2019-02-12 14:36:30 -0500

    Fixed issue with line breaks posting notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,8 @@ class NotesController < ApplicationController
     @note = @notable.notes.new note_params
     @note.user = current_user
 
+    @body = @note.body.gsub(/(?:\n\r?|\r\n?)/, '<br>').html_safe
+
     # Save the note
     @note.save
 
