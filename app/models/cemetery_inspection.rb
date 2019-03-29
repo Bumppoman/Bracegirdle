fa4f84d83ff79883ccea882f15760a4b18b58a23
Brendon Stanton <brendonstanton@icloud.com>
2019-03-29 14:27:07 -0400

    Small changes in displaying list of cemetery inspections

--- app/models/cemetery_inspection.rb
+++ app/models/cemetery_inspection.rb
@@ -7,10 +7,23 @@ class CemeteryInspection < ApplicationRecord
 
   has_one_attached :inspection_report
 
+  NAMED_STATUSES = {
+      1 => 'Performed',
+      4 => 'Complete'
+  }.freeze
+
   STATUSES = {
     performed: 1,
     complete: 4
   }.freeze
+
+  def named_status
+    NAMED_STATUSES[status]
+  end
+
+  def score
+    return '---' if inspection_report.attached?
+  end
 
   def to_param
     date_performed.strftime('%Y-%m-%d')
