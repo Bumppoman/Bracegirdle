d473b63c5f51895d87ad14fd934c865299cf5ceb
Brendon Stanton <brendonstanton@icloud.com>
2019-04-01 09:12:36 -0400

    Display tweak for last inspection on cemetery info page

--- app/helpers/cemeteries_helper.rb
+++ app/helpers/cemeteries_helper.rb
@@ -1,2 +1,11 @@
 module CemeteriesHelper
+  def formatted_last_inspection(cemetery)
+    if cemetery.last_inspection
+      "#{cemetery.last_inspection_date} (#{link_to 'view', show_inspection_cemetery_path(date: cemetery.last_inspection)})"
+    elsif cemetery.last_inspection_date
+      cemetery.last_inspection_date
+    else
+      'No inspection recorded'
+    end
+  end
 end
--- app/models/cemetery.rb
+++ app/models/cemetery.rb
@@ -38,16 +38,6 @@ class Cemetery < ApplicationRecord
     COUNTIES[county]
   end
 
-  def formatted_last_inspection
-    if last_inspection
-      "#{@cemetery.last_inspection_date} (#{link_to 'view', show_inspection_cemetery_path(date: @cemetery.last_inspection)})"
-    elsif last_inspection_date
-      last_inspection_date
-    else
-      'No inspection recorded'
-    end
-  end
-
   def investigator
     User.find_by_region(region :investigator)
   end
