a9aac3691bf422255ba1ff9dc695514bc134af4d
Brendon Stanton <brendonstanton@icloud.com>
2019-04-04 10:15:06 -0400

    A couple small tweaks

--- app/helpers/cemeteries_helper.rb
+++ app/helpers/cemeteries_helper.rb
@@ -1,7 +1,7 @@
 module CemeteriesHelper
   def formatted_last_inspection(cemetery)
     if cemetery.last_inspection_date
-      output = cemetery.last_inspection_date
+      output = cemetery.last_inspection_date.to_s
       output << " (#{link_to 'view', show_inspection_cemetery_path(date: cemetery.last_inspection)})" if cemetery.last_inspection
     else
       output = 'No inspection recorded'
