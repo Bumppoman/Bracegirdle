d8f85d2c7c94a59849236f19a2e5bdaf208ea86e
Brendon Stanton <brendonstanton@icloud.com>
2019-05-01 19:59:38 -0400

    Incomplete inspection list; link to begin inspection

--- app/helpers/cemeteries_helper.rb
+++ app/helpers/cemeteries_helper.rb
@@ -2,7 +2,7 @@ module CemeteriesHelper
   def formatted_last_inspection(cemetery)
     if cemetery.last_inspection_date
       output = cemetery.last_inspection_date.to_s
-      output << " (#{link_to 'view', show_inspection_cemetery_path(uuid: cemetery.last_inspection)})" if cemetery.last_inspection
+      output << " (#{link_to 'view', show_inspection_cemetery_path(identifier: cemetery.last_inspection)})" if cemetery.last_inspection
     else
       output = 'No inspection recorded'
     end
