96350fd45e0be14d1eef76cb4e8f6811211ad2f8
Brendon Stanton <brendonstanton@icloud.com>
2021-05-20 10:25:27 -0400

    Update JS dependencies

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,22 @@
 # config valid for current version and patch releases of Capistrano
-#lock "~> 3.12.0"
+lock "~> 3.16.0"
 
 set :application, "Bracegirdle"
 set :repo_url, "git@github.com:Bumppoman/Bracegirdle.git"
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-
-set :deploy_to, "/var/www/bracegirdle"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :puma_bind,       "#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf,       "#{shared_path}/puma.rb"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{shared_path}/log/puma.error.log"
-set :puma_error_log,  "#{shared_path}/log/puma.access.log"
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/bracegirdle"
 
-# SSH
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
 if File.exist?("config/deploy_id_rsa")
   set :ssh_options, {
     forward_agent: true,
@@ -27,17 +25,6 @@ if File.exist?("config/deploy_id_rsa")
 else
   set :ssh_options, { forward_agent: true }
 end
-
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
@@ -50,7 +37,7 @@ append :linked_files, 'config/master.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'public/packs', 'node_modules', 'app/pdfs/signatures'
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { 'NODE_ENV' => 'production' }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
@@ -58,49 +45,5 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :default_env, { 'NODE_ENV' => 'production' }
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute 'sudo systemctl restart bracegirdle-puma.service'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
